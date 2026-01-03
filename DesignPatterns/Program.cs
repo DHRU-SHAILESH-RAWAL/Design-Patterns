63fa2185768e623eda49dd2f874813f05a49a132
DHRU-SHAILESH-RAWAL <dhrushaileshbhairawal@gmail.com>
2026-01-03 21:25:11 +0530

    Updated folder structure will update l;ater with creational design pattern new folder

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,6 +1,6 @@
-﻿using Design_Patterns.SingletonPattern;
+﻿////using Design_Patterns.SingletonPattern;
 
-Console.WriteLine("Desgn Patterns Commanly used in C#");
-SingletonLazy singletonLazy = SingletonLazy.GetSingletonInstance;
+////Console.WriteLine("Desgn Patterns Commanly used in C#");
+////SingletonLazy singletonLazy = SingletonLazy.GetSingletonInstance;
 
-SingletonEager singletonEager = SingletonEager.GetInstance;
+////SingletonEager singletonEager = SingletonEager.GetInstance;
